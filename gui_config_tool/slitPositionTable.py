ab8a9b8cb5d819e548f9217809e5262ec9956f4c
Austin Bowman <astrangbow2@gmail.com>
2025-07-14 15:13:24 -0700

    initial commit

---
+++ gui_config_tool/slitPositionTable.py
@@ -0,0 +1,7 @@
+"""
+this will display all the slit positions as well as the bar number and then width
+additionally, when you click on the slits, it will highlight the corresponding bar in the 
+interactive image and highlight the corresponding star in the target list table
+"""
+
+
