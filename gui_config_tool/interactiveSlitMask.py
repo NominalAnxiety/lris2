9d129c9e65e67c7b71864af4cd20b896d3d9f305
Austin Bowman <astrangbow2@gmail.com>
2025-07-14 17:09:15 -0700

    slightly edited the interactiveslitmask file to take out unneccesary code

--- gui_config_tool/interactiveSlitMask.py
+++ gui_config_tool/interactiveSlitMask.py
@@ -4,7 +4,7 @@ when you click the bar on the left then the image will display which row that is
 additionally It will also interact with the target list
 it will display where the slit is place and what stars will be shown
 """
-from PyQt6.QtCore import Qt, pyqtSlot, QPointF
+from PyQt6.QtCore import Qt, pyqtSlot, QLineF, QParallelAnimationGroup, QPropertyAnimation, QMetaProperty, QPointF
 from PyQt6.QtGui import QBrush, QPen, QPainter, QColor, QFont
 from PyQt6.QtWidgets import (
     QApplication,
@@ -65,6 +65,7 @@ class interactiveSlits(QGraphicsItemGroup):
         #will have default lines down the middle
         #default NONE next to lines that don't have a star
         self.line = QGraphicsLineItem(x,y,x,y+7)
+        #self.line = QLineF(x,y,x,y+7)
         self.line.setPen(QPen(Qt.GlobalColor.red, 2))
 
         self.star = QGraphicsTextItem("NONE")
@@ -75,46 +76,25 @@ class interactiveSlits(QGraphicsItemGroup):
         self.addToGroup(self.line)
         self.addToGroup(self.star)
 
-    @pyqtSlot(float,name="slit position")
-    def change_position(self,pos):
-        #assuming position is a tuple (x,y) (pos = position)
-        self.line = QGraphicsLineItem(pos[0],pos[1],pos[0],pos[1]+7)
-        self.star.setPos(pos[0]+5,pos[1]-4)
-        #update the line position and the label position
 
-    @pyqtSlot(str,name="star name")
-    def change_star(self,star_name):
-        self.star.setPlainText(star_name)
-
+    
+#random line position maker
         
 
 class interactiveSlitMask(QWidget):
     def __init__(self):
         super().__init__()
         #this will display the image
+        #I think it would be cool to make the bars on the GUI move instead of just the slits moving
         self.scene = QGraphicsScene(0,0,480,520)
-        
-        master_rect = interactiveBars(0,7,0)
-        master_line = interactiveSlits(240,7)
-
-        rect_list = []
-        line_list = []
 
         for i in range(72):
-            rect_list.append(interactiveBars(0,i*7+7,i))
+            temp_rect = interactiveBars(0,i*7+7,i)
+            self.scene.addItem(temp_rect)
         for i in range(72):
-            line_list.append(interactiveSlits(240,7*i+7))
+            temp_slit = interactiveSlits(240,7*i+7)
+            self.scene.addItem(temp_slit)
 
-        self.scene.addItem(master_rect)
-        
-
-        for i in range(1,72):
-            self.scene.addItem(rect_list[i])
-
-        self.scene.addItem(master_line)
-        for i in range(1,72):
-            self.scene.addItem(line_list[i])
-        
         view = QGraphicsView(self.scene)
         view.setRenderHint(QPainter.RenderHint.Antialiasing)
 
@@ -123,4 +103,19 @@ class interactiveSlitMask(QWidget):
 
 
         self.setLayout(layout)
+
+    @pyqtSlot(float,name="slit position")
+    def slit_and_name_animation(self,pos):
+
+        for item in self.scene.items():
+            if isinstance(item, QGraphicsItemGroup):
+                print('hi')
+
+
+        pass
+
+    @pyqtSlot(str,name="star name")
+    def change_star(self,star_name):
+        #self.star.setPlainText(star_name)
+        pass
 
