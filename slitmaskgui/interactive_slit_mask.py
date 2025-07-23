6aed603871732434cae025ab58f6de0e0fac9ba0
Austin Bowman <astrangbow2@gmail.com>
2025-07-23 11:39:55 -0700

    the slit_mask now scales when the area is changed by the user

--- slitmaskgui/interactive_slit_mask.py
+++ slitmaskgui/interactive_slit_mask.py
@@ -5,7 +5,7 @@ additionally It will also interact with the target list
 it will display where the slit is place and what stars will be shown
 """
 from PyQt6.QtCore import Qt, pyqtSlot, pyqtSignal, QSize
-from PyQt6.QtGui import QBrush, QPen, QPainter, QColor, QFont
+from PyQt6.QtGui import QBrush, QPen, QPainter, QColor, QFont, QTransform
 from PyQt6.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
@@ -30,10 +30,12 @@ CSU_WIDTH = PLATE_SCALE*60*5 #width of the csu in mm (widgth is 5 arcmin)
 
 class interactiveBars(QGraphicsRectItem):
     
-    def __init__(self,x,y,this_id):
+    def __init__(self,x,y,bar_length,bar_width,this_id):
         super().__init__()
         #creates a rectangle that can cha
-        self.setRect(x,y, 480,7)
+        self.length = bar_length
+        self.width = bar_width
+        self.setRect(x,y, self.length,self.width)
         self.id = this_id
         self.setBrush = QBrush(Qt.GlobalColor.white)
         self.setPen = QPen(Qt.GlobalColor.black).setWidth(1)
@@ -52,6 +54,9 @@ class interactiveBars(QGraphicsRectItem):
             painter.setBrush(QBrush(Qt.GlobalColor.white))
             painter.setPen(QPen(QColor("black"), 1))
         painter.drawRect(self.rect())
+    
+    def send_size(self):
+        return (self.length,self.width)
 
 class FieldOfView(QGraphicsRectItem):
     def __init__(self,image_height,x=0,y=0):
@@ -94,38 +99,70 @@ class interactiveSlits(QGraphicsItemGroup):
     def get_y_value(self):
         return self.y_pos
 
+class CustomGraphicsView(QGraphicsView):
+    def __init__(self,scene):
+        super().__init__(scene)
+        # self.scene() == scene
+        self.previous_height = self.height()
+        self.previous_width = self.width()
+    
+        self.scale(1,0.9)
+
+    def resizeEvent(self,event):
+        new_width = self.size().width()
+        new_height = self.size().height()
+
+        scale_x = new_width / self.previous_width
+        scale_y = new_height / self.previous_height
+
+        self.scale(scale_x, scale_y)
+
+        self.previous_width = new_width
+        self.previous_height = new_height
+
+        super().resizeEvent(event)
+
+    def sizePolicy(self):
+        return super().sizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+    def renderHints(self):
+        return super().renderHints(QPainter.RenderHint.Antialiasing)
+
 class interactiveSlitMask(QWidget):
     row_selected = pyqtSignal(int,name="row selected")
     def __init__(self):
         super().__init__()
         #this will display the image
         #I think it would be cool to make the bars on the GUI move instead of just the slits moving
-        self.scene = QGraphicsScene(0,0,480,520)
+        scene_width = 480
+        scene_height = 520
+        self.scene = QGraphicsScene(0,0,scene_width,scene_height)
 
-        self.setSizePolicy(
-            QSizePolicy.Policy.MinimumExpanding,
-            QSizePolicy.Policy.MinimumExpanding
-        )
+        # self.setSizePolicy(
+        #     QSizePolicy.Policy.MinimumExpanding,
+        #     QSizePolicy.Policy.MinimumExpanding
+        # )
         height = self.height() #this is the height of the widget
         width = self.width()
         total_height_of_bars = 7*72
         xcenter_of_image = self.scene.width()/2
         
-        
+        initial_bar_width = 7
+        initial_bar_length = 480
 
         for i in range(72):
-            temp_rect = interactiveBars(0,i*7+7,i)
+            temp_rect = interactiveBars(0,i*7+7,this_id=i,bar_width=initial_bar_width,bar_length=initial_bar_length)
             self.scene.addItem(temp_rect)
         for i in range(72):
-            temp_slit = interactiveSlits(240,7*i+7)
+            temp_slit = interactiveSlits(scene_width/2,7*i+7)
             self.scene.addItem(temp_slit)
         fov = FieldOfView(total_height_of_bars,x=xcenter_of_image/2,y=7)
         self.scene.addItem(fov)
 
+        self.view = CustomGraphicsView(self.scene)
+        # self.view = QGraphicsView(self.scene)
+        # self.view.setRenderHint(QPainter.RenderHint.Antialiasing)
+        # self.view.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding)
 
-        self.view = QGraphicsView(self.scene)
-        self.view.setRenderHint(QPainter.RenderHint.Antialiasing)
-        self.view.setSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.MinimumExpanding)
 
 
         self.scene.selectionChanged.connect(self.row_is_selected)
@@ -142,7 +179,7 @@ class interactiveSlitMask(QWidget):
         self.setLayout(main_layout)
     
     def sizeHint(self):
-        return QSize(550,570)
+        return QSize(650,620)
     
     @pyqtSlot(int,name="row selected")
     def select_corresponding_row(self,row):
